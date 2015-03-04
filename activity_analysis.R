9e0b1c518f5635cd759e26e19a0e552ffe314b3e
Doug Fleming <doug@westfork.net>
2015-03-04 15:56:07 -0700

    updates to populate NA with mean by interval by weekday type

--- activity_analysis.R
+++ activity_analysis.R
@@ -14,6 +14,7 @@ if (!file.exists(activityFile)) {
 }
 
 data <- read.csv(unz(activityFile,"activity.csv"))
+data <- mutate(data, daytype = ifelse(wday(date, label=T) %in% c("Sat","Sun"),'weekend','weekday'))
 
 totStepsPerDay <- data[!is.na(data$steps),] %>%
   group_by(date) %>%
@@ -33,8 +34,18 @@ with(meanByInterval,plot(interval, steps, type="l", ylab="Steps", xlab = "Interv
 
 #handle NA update with mutate or sqldf update?
 # mutate(dat, dist = ifelse(speed == 4, dist * 100, dist)
+#update NA to mean by weekday, interval 
 
-meanByInterval[meanByInterval$steps == max(meanByInterval$steps),]
+meanByIntervalDayType <- data[!is.na(data$steps),] %>%
+  group_by(interval, daytype) %>%
+  summarise_each_(funs(mean), vars = c("interval","steps","daytype"))
 
-weekend <- data[wday(data$date, label=T) %in% c("Sat","Sun"),]
-weekday <- data[!wday(data$date, label=T) %in% c("Sat","Sun"),]
+d2<- left_join(meanByIntervalDayType, data, by=c("interval","daytype"))
+data[is.na(data$steps),]$steps <- d2[is.na(d2$steps.y),]$steps.x
+
+
+
+
+
+
+
