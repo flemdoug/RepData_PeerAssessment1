e5168f2bb7dcd3ddc2f14b7081e5880a672a2524
Doug Fleming <doug@westfork.net>
2015-03-05 06:49:55 -0700

    Another method to add values to NA

--- activity_analysis.R
+++ activity_analysis.R
@@ -15,6 +15,7 @@ if (!file.exists(activityFile)) {
 
 data <- read.csv(unz(activityFile,"activity.csv"))
 data <- mutate(data, daytype = ifelse(wday(date, label=T) %in% c("Sat","Sun"),'weekend','weekday'))
+data <- mutate(data, dow = wday(date, label=T))
 
 totStepsPerDay <- data[!is.na(data$steps),] %>%
   group_by(date) %>%
@@ -24,29 +25,39 @@ totStepsPerDay <- data[!is.na(data$steps),] %>%
 medianStepsPerDay <- median(totStepsPerDay$totSteps)
 meanStepsPerDay <- mean(totStepsPerDay$totSteps) 
 
-hist(data$steps)
-
 meanByInterval <- data[!is.na(data$steps),] %>%
   group_by(interval) %>%
   summarise_each_(funs(mean), vars = c("interval","steps"))
-
-with(meanByInterval,plot(interval, steps, type="l", ylab="Steps", xlab = "Interval"))
 
 meanByIntervalDayType <- data[!is.na(data$steps),] %>%
   group_by(interval, daytype) %>%
   summarise_each_(funs(mean), vars = c("interval","steps","daytype"))
 
-meanByIntervalDayType$steps <- meanByIntervalDayType$steps
+meanByIntervalDOW <- data[!is.na(data$steps),] %>%
+  group_by(interval, dow) %>%
+  summarise_each_(funs(mean), vars = c("interval","steps","dow"))
 
-for ( i in unique(data$interval)) {
-  data[is.na(data$steps) & data$interval == i & data$daytype == 'weekday',]$steps <- round(meanByIntervalDayType[meanByIntervalDayType$interval == i & meanByIntervalDayType$daytype == 'weekday',]$steps,0)
+dataDOW <- data
+dataDateType <- data
+
+for (i in unique(dataDateType$interval)) {
+  dataDateType[is.na(dataDateType$steps) & dataDateType$interval == i & dataDateType$daytype == 'weekday',]$steps <- round(meanByIntervalDayType[meanByIntervalDayType$interval == i & meanByIntervalDayType$daytype == 'weekday',]$steps,0)
+  dataDateType[is.na(dataDateType$steps) & dataDateType$interval == i & dataDateType$daytype == 'weekend',]$steps <- round(meanByIntervalDayType[meanByIntervalDayType$interval == i & meanByIntervalDayType$daytype == 'weekend',]$steps,0)
 }
 
-for ( i in unique(data$interval)) {
-  data[is.na(data$steps) & data$interval == i & data$daytype == 'weekend',]$steps <- round(meanByIntervalDayType[meanByIntervalDayType$interval == i & meanByIntervalDayType$daytype == 'weekend',]$steps,0)
+
+for (i in unique(dataDOW$interval)) {
+  for (j in c("Mon","Tue","Wed","Thurs","Fri","Sat","Sun")) {
+    dataDOW[is.na(dataDOW$steps) & dataDOW$interval == i & dataDOW$dow == j,]$steps <- round(meanByIntervalDOW[meanByIntervalDOW$interval == i & meanByIntervalDOW$dow == j, ]$steps,0)
+  }
 }
 
+data$StepsIntDayType <- dataDateType$steps
+data$StepsIntDOW <- dataDOW$steps
+rm(dataDateType, dataDOW)
 
+hist(data$steps)
+with(meanByInterval,plot(interval, steps, type="l", ylab="Steps", xlab = "Interval"))
 
-
+hist(data$StepsIntDayType)
 
